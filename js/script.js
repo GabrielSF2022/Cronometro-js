0f012fad70159e3daa6803579f98e3fda0822d30
GABRIEL DE SOUZA FURTADO <97768716+GabrielSF2022@users.noreply.github.com>
2022-04-28 09:38:28 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+var sec = 0;
+var min = 0;
+var hours = 0;
+var interval;
+
+
+function watch() {
+    sec++;
+    if (sec == 60) {
+      min++;
+      sec = 0;
+    }
+  
+    if (min == 60) {
+      hours++;
+      min = 0;
+    }
+    let n1 = document.getElementById("watch");
+    n1.innerText = twoDigits(hours) + ":" + twoDigits(min) + ":" + twoDigits(sec);
+  }
+  
+
+function twoDigits(digit) {
+  if (digit < 10) {
+    return "0" + digit;
+  } else {
+    return digit;
+  }
+}
+
+function iniciar() {
+  watch();
+  interval = setInterval(watch, 1000);
+}
+
+function parar() {
+  clearInterval(interval);
+}
+
+function limpar() {
+  clearInterval(interval);
+  sec = 0;
+  min = 0;
+  hours = 0;
+
+  document.getElementById("watch").innerText = "00:00:00";
+}
+
+
+
